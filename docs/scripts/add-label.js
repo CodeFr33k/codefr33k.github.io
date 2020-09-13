8f9a5f3dde6e98cf5927f2a0f0fd28d86d6addb1
CodeFr33k <noreply>
2020-09-13 21:31:37 +0930

    deploy

--- docs/scripts/add-label.js
+++ docs/scripts/add-label.js
@@ -1,13 +1,9 @@
 function(records) {
     for(const record of records) {
-        for(const part of record.parts) {
-            for(const annotation of part.annotations) {
-                if(annotation.key !== 'day label') {
-                    continue;
-                }
-                record.userData.push(annotation);
-            }
-        }
+        record.userData.push(new KeyValue(
+            'day label',
+            record.parts[0].lines[0]
+        ));
     }
     return records;
 }
